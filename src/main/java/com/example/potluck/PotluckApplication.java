b1fc0c4df7d99757fad768f75df685a39eac8436
Habibul Ali Shah <alishah730@gmail.com>
2017-10-01 19:55:28 +0530

    Packege Renamed
    Base package renamed. Example removed.

--- src/main/java/com/example/potluck/PotluckApplication.java
+++ src/main/java/com/example/potluck/PotluckApplication.java
@@ -1,12 +1,12 @@
-package com.example.demo;
+package com.example.potluck;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class PotluckApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(PotluckApplication.class, args);
 	}
 }
